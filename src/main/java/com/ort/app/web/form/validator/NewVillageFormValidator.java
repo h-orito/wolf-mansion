00b122a1e4e389ed87f5981ddd3d7c5c0ccb4af8
h-orito <16453967+h-orito@users.noreply.github.com>
2018-04-19 10:45:04 +0900

    構成の入力チェック実装

--- src/main/java/com/ort/app/web/form/validator/NewVillageFormValidator.java
+++ src/main/java/com/ort/app/web/form/validator/NewVillageFormValidator.java
@@ -2,6 +2,8 @@ package com.ort.app.web.form.validator;
 
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,9 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import com.ort.app.web.form.NewVillageForm;
+import com.ort.app.web.util.SkillUtil;
+import com.ort.dbflute.allcommon.CDef;
+import com.ort.dbflute.allcommon.CDef.Skill;
 import com.ort.fw.util.WerewolfMansionDateUtil;
 
 @Component
@@ -59,6 +64,22 @@ public class NewVillageFormValidator implements Validator {
         }
 
         // ダミーキャラ発言
+        validateMessage(errors, form);
+
+        // 入村パスワードが入力されているが3文字以上12文字以下でない
+        String joinPassword = form.getJoinPassword();
+        if (StringUtils.isNotEmpty(joinPassword)) {
+            if (joinPassword.length() < 3 || 12 < joinPassword.length()) {
+                errors.rejectValue("joinPassword", "VillageSayForm.validator.joinPassword.length");
+            }
+        }
+
+        // 構成
+        validateOrganization(errors, form);
+
+    }
+
+    private void validateMessage(Errors errors, NewVillageForm form) {
         String message = form.getDummyJoinMessage();
         if (message == null) {
             return;
@@ -76,13 +97,102 @@ public class NewVillageFormValidator implements Validator {
         if (trimedMessage.split("\r\n").length > 10) {
             errors.rejectValue("dummyJoinMessage", "VillageSayForm.validator.message.line");
         }
+    }
 
-        // 入村パスワードが入力されているが3文字以上12文字以下でない
-        String joinPassword = form.getJoinPassword();
-        if (StringUtils.isNotEmpty(joinPassword)) {
-            if (joinPassword.length() < 3 || 12 < joinPassword.length()) {
-                errors.rejectValue("joinPassword", "VillageSayForm.validator.joinPassword.length");
-            }
+    private void validateOrganization(Errors errors, NewVillageForm form) {
+        String organization = form.getOrganization();
+        // 未入力
+        if (StringUtils.isEmpty(organization)) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.required");
+            return;
         }
+        // 行数不足
+        String[] organizationArray = organization.replace("\r\n", "\n").split("\n");
+        if (organizationArray.length < 13) { // 8~20名
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.lines");
+            return;
+        }
+        // 行ごとにチェック
+        int personNum = 8;
+        for (String org : organizationArray) {
+            int lineNum = personNum - 7;
+            // 人数過不足
+            if (org.length() != personNum) {
+                errors.rejectValue("organization", "VillageSayForm.validator.organization.personNum", new Object[] { lineNum, personNum },
+                        null);
+                return;
+            }
+            // 存在しない役職
+            for (String character : org.split("")) {
+                if (!SkillUtil.SKILL_SHORTNAME_MAP.containsKey(character)) {
+                    errors.rejectValue("organization", "VillageSayForm.validator.organization.noexistskill",
+                            new Object[] { lineNum, character }, null);
+                    return;
+                }
+            }
+            // 役職人数制限
+            if (isInvalidOrganizationSkillPersonNum(errors, org, lineNum)) {
+                return;
+            }
+
+            personNum++;
+        }
+    }
+
+    private boolean isInvalidOrganizationSkillPersonNum(Errors errors, String org, int lineNum) {
+        Map<CDef.Skill, Integer> skillPersonNumMap = createSkillPersonNum(org);
+        // 占い師と賢者は1名まで
+        if (skillPersonNumMap.get(CDef.Skill.占い師) + skillPersonNumMap.get(CDef.Skill.賢者) > 1) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.maxperson", new Object[] { "占い師と賢者", 1, lineNum },
+                    null);
+            return true;
+        }
+        // 狂人と魔神官とC国狂人は最大1名まで
+        if (skillPersonNumMap.get(CDef.Skill.狂人) + skillPersonNumMap.get(CDef.Skill.魔神官) + skillPersonNumMap.get(CDef.Skill.C国狂人) > 1) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.maxperson",
+                    new Object[] { "狂人と魔神官とC国狂人", 1, lineNum }, null);
+            return true;
+        }
+        // 狩人は最大1名まで
+        if (skillPersonNumMap.get(CDef.Skill.狩人) > 1) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.maxperson", new Object[] { "狩人", 1, lineNum }, null);
+            return true;
+        }
+        // 妖狐は最大1名まで
+        if (skillPersonNumMap.get(CDef.Skill.妖狐) > 1) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.maxperson", new Object[] { "妖狐", 1, lineNum }, null);
+            return true;
+        }
+        // 村人がいない
+        if (skillPersonNumMap.get(CDef.Skill.村人) < 1) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.noexistvillager", new Object[] { lineNum }, null);
+            return true;
+        }
+        // 人狼がいない
+        if (skillPersonNumMap.get(CDef.Skill.人狼) < 1) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.noexistwerewolf", new Object[] { lineNum }, null);
+            return true;
+        }
+        // 人狼の人数が過半数を超えている
+        if (skillPersonNumMap.get(CDef.Skill.人狼) > org.length() / 2) {
+            errors.rejectValue("organization", "VillageSayForm.validator.organization.werewolfwin", new Object[] { lineNum }, null);
+            return true;
+        }
+
+        return false;
+    }
+
+    private Map<Skill, Integer> createSkillPersonNum(String org) {
+        Map<CDef.Skill, Integer> skillPersonNumMap = new HashMap<>();
+        for (CDef.Skill skill : CDef.Skill.values()) {
+            skillPersonNumMap.put(skill, 0);
+        }
+        for (String character : org.split("")) {
+            CDef.Skill skill = SkillUtil.SKILL_SHORTNAME_MAP.get(character);
+            Integer skillPersonNum = skillPersonNumMap.get(skill);
+            skillPersonNum++;
+            skillPersonNumMap.put(skill, skillPersonNum);
+        }
+        return skillPersonNumMap;
     }
 }
--- src/main/java/com/ort/app/web/util/SkillUtil.java
+++ src/main/java/com/ort/app/web/util/SkillUtil.java
@@ -1,8 +1,31 @@
 package com.ort.app.web.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections4.MapUtils;
+
 import com.ort.dbflute.allcommon.CDef;
 
 public class SkillUtil {
+
+    public static Map<String, CDef.Skill> SKILL_SHORTNAME_MAP;
+    static {
+        Map<String, CDef.Skill> map = new HashMap<>();
+        map.put("村", CDef.Skill.村人);
+        map.put("狼", CDef.Skill.人狼);
+        map.put("狂", CDef.Skill.狂人);
+        map.put("C", CDef.Skill.C国狂人);
+        map.put("魔", CDef.Skill.魔神官);
+        map.put("占", CDef.Skill.占い師);
+        map.put("賢", CDef.Skill.賢者);
+        map.put("霊", CDef.Skill.霊能者);
+        map.put("導", CDef.Skill.導師);
+        map.put("狐", CDef.Skill.妖狐);
+        map.put("共", CDef.Skill.共鳴者);
+        map.put("狩", CDef.Skill.狩人);
+        SKILL_SHORTNAME_MAP = MapUtils.unmodifiableMap(map);
+    }
 
     private SkillUtil() {
     }
