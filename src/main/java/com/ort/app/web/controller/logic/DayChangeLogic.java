2d287e29c4ac40eef0423713f2c3870f73a54d00
h-orito <16453967+h-orito@users.noreply.github.com>
2020-01-31 23:35:10 +0900

    バグ修正　左右の部屋を貫通して占星術師が占えた不具合を修正

--- src/main/java/com/ort/app/web/controller/logic/DayChangeLogic.java
+++ src/main/java/com/ort/app/web/controller/logic/DayChangeLogic.java
@@ -998,17 +998,45 @@ public class DayChangeLogic {
 
     // 対象＋周辺8部屋の部屋番号（存在しない部屋番号を含んでいても良い）
     private List<Integer> detectAroundRoomNumber(Integer targetRoomNumber, Integer roomSizeWidth) {
-        return Arrays.asList(//
-                targetRoomNumber, // 対象の部屋
-                targetRoomNumber - roomSizeWidth - 1, // 左上
-                targetRoomNumber - roomSizeWidth, // 上
-                targetRoomNumber - roomSizeWidth + 1, // 右上
-                targetRoomNumber - 1, // 左
-                targetRoomNumber + 1, // 右
-                targetRoomNumber + roomSizeWidth - 1, // 左下
-                targetRoomNumber + roomSizeWidth, // 下
-                targetRoomNumber + roomSizeWidth + 1 // 右下
-        );
+        if (isLeftSide(targetRoomNumber, roomSizeWidth)) {
+            return Arrays.asList(//
+                    targetRoomNumber, // 対象の部屋
+                    targetRoomNumber - roomSizeWidth, // 上
+                    targetRoomNumber - roomSizeWidth + 1, // 右上
+                    targetRoomNumber + 1, // 右
+                    targetRoomNumber + roomSizeWidth, // 下
+                    targetRoomNumber + roomSizeWidth + 1 // 右下
+            );
+        } else if (isRightSide(targetRoomNumber, roomSizeWidth)) {
+            return Arrays.asList(//
+                    targetRoomNumber, // 対象の部屋
+                    targetRoomNumber - roomSizeWidth - 1, // 左上
+                    targetRoomNumber - roomSizeWidth, // 上
+                    targetRoomNumber - 1, // 左
+                    targetRoomNumber + roomSizeWidth - 1, // 左下
+                    targetRoomNumber + roomSizeWidth // 下
+            );
+        } else {
+            return Arrays.asList(//
+                    targetRoomNumber, // 対象の部屋
+                    targetRoomNumber - roomSizeWidth - 1, // 左上
+                    targetRoomNumber - roomSizeWidth, // 上
+                    targetRoomNumber - roomSizeWidth + 1, // 右上
+                    targetRoomNumber - 1, // 左
+                    targetRoomNumber + 1, // 右
+                    targetRoomNumber + roomSizeWidth - 1, // 左下
+                    targetRoomNumber + roomSizeWidth, // 下
+                    targetRoomNumber + roomSizeWidth + 1 // 右下
+            );
+        }
+    }
+
+    private boolean isLeftSide(int targetRoomNumber, int width) {
+        return targetRoomNumber % width == 1;
+    }
+
+    private boolean isRightSide(int targetRoomNumber, int width) {
+        return targetRoomNumber % width == 0;
     }
 
     // 護衛
