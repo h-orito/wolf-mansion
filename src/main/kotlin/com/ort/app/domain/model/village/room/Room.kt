4008e477df214e50ac3a9fd416666f80a0f86118
h-orito <16453967+h-orito@users.noreply.github.com>
2021-10-28 09:03:55 +0900

    1日目にフルバして2日目に探偵調査すると部屋が正常に取得できなくてエラーになる不具合修正

--- src/main/kotlin/com/ort/app/domain/model/village/room/Room.kt
+++ src/main/kotlin/com/ort/app/domain/model/village/room/Room.kt
@@ -16,8 +16,9 @@ data class Room(
 
     // フルーツバスケット等があった場合は移動後が取得される
     fun numberWhen(day: Int): Int? {
+        val d = if (day <= 1) 1 else day // 0日目を指定すると部屋が取れない
         if (histories.list.isEmpty()) return null
-        val maxDay = histories.list.filter { it.day <= day }.map { it.day }.max() ?: return null
+        val maxDay = histories.list.filter { it.day <= day }.map { it.day }.maxOrNull() ?: return null
         return histories.list.lastOrNull { it.day == maxDay }?.number
     }
 
