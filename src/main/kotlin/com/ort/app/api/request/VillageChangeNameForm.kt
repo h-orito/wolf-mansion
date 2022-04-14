2892f55dfd57bb2df59adf136671a7069a1cfbae
h-orito <16453967+h-orito@users.noreply.github.com>
2022-04-14 11:46:16 +0900

    名前変更で空文字が入力されるとエラーになる不具合修正

--- src/main/kotlin/com/ort/app/api/request/VillageChangeNameForm.kt
+++ src/main/kotlin/com/ort/app/api/request/VillageChangeNameForm.kt
@@ -1,14 +1,15 @@
 package com.ort.app.api.request
 
 import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 data class VillageChangeNameForm(
-    @field:NotNull
+    @field:NotBlank
     @field:Length(min = 1, max = 40)
     val name: String? = null,
 
-    @field:NotNull
+    @field:NotBlank
     @field:Length(min = 1, max = 1)
     val shortName: String? = null
 )
