70833625d86895e4ab60575ba129ba77ba56d511
h-orito <16453967+h-orito@users.noreply.github.com>
2022-04-21 23:49:06 +0900

    refactor

--- src/main/resources/static/app/js/village.js
+++ src/main/resources/static/app/js/village.js
@@ -1591,14 +1591,5 @@ $(function () {
         const input = $(this)
         const label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
         input.parent().parent().next(':text').val(label);
-//        const files = !!this.files ? this.files : [];
-//        if (!files.length || !window.FileReader) return;
-//        if (/^image/.test(files[0].type)){
-//            var reader = new FileReader();
-//            reader.readAsDataURL(files[0]);
-//            reader.onloadend = function(){
-//                $('#dummy-chara-img').attr('src', this.result);
-//            }
-//        }
     });
 });
