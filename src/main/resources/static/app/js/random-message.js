76484b99abe64b7d3c8d15aed03059cdc37dcc5a
h-orito <16453967+h-orito@users.noreply.github.com>
2022-10-28 10:45:28 +0900

    キーワード検索を追加

--- src/main/resources/static/app/js/random-message.js
+++ src/main/resources/static/app/js/random-message.js
@@ -7,4 +7,42 @@ $(function() {
 		});
 		$(this).remove();
 	});
+
+	$('body').on('click', '[data-search]', function(){
+		const keyword = $('#search-keyword').val();
+		if (keyword.length <= 0) {
+			$('#keyword-table').find('tbody tr').each(function(){
+				$(this).removeClass('hidden');
+			});
+			return;
+		}
+
+		$('#keyword-table').find('tbody tr').each(function(){
+			const word = $(this).find('td').eq(0).text();
+			if (word.indexOf(keyword) != -1) {
+				$(this).removeClass('hidden');
+			} else {
+				$(this).addClass('hidden');
+			}
+		});
+	});
+
+	$('body').on('click', '[data-copy]', function(){
+		const text = '[[' + $(this).data('copy') + ']]';
+		clipboardCopy(text, 'コピーしました： ' + text);
+	});
+
+	// コピー
+	function clipboardCopy(text, alertText) {
+		var temp = document.createElement('div');
+		temp.appendChild(document.createElement('pre')).textContent = text;
+		var s = temp.style;
+		s.position = 'fixed';
+		s.left = '-100%';
+		document.body.appendChild(temp);
+		document.getSelection().selectAllChildren(temp);
+		var result = document.execCommand('copy');
+		document.body.removeChild(temp);
+		alert(alertText);
+	}
 });
