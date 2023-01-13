cea88640ab188559bd799ef44ac13232333aee34
h-orito <16453967+h-orito@users.noreply.github.com>
2023-01-13 14:50:04 +0900

    作成できるIDと齟齬があった

--- src/main/kotlin/com/ort/app/api/request/LoginForm.kt
+++ src/main/kotlin/com/ort/app/api/request/LoginForm.kt
@@ -8,7 +8,7 @@ data class LoginForm(
     /** ユーザID  */
     @field:NotNull
     @field:Length(min = 3, max = 12)
-    @field:Pattern(regexp = "[a-zA-Z0-9]*")
+    @field:Pattern(regexp = "[a-zA-Z][a-zA-Z0-9\\-_]*")
     val userId: String? = null,
 
     /** パスワード  */
