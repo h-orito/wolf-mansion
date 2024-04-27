082dfa5b220dae50945baa89b9920baf12cf7c2e
h-orito <16453967+h-orito@users.noreply.github.com>
2024-04-27 22:57:17 +0900

    最終アクセス日時をわかりやすくする

--- src/main/kotlin/com/ort/app/api/view/village/VillageMemberContent.kt
+++ src/main/kotlin/com/ort/app/api/view/village/VillageMemberContent.kt
@@ -3,6 +3,8 @@ package com.ort.app.api.view.village
 import com.ort.app.domain.model.village.participant.VillageParticipant
 import com.ort.app.domain.model.village.participant.VillageParticipants
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 data class VillageMemberContent(
     /** 状態 e.g. 襲撃 */
@@ -32,6 +34,8 @@ data class VillageMemberContent(
         val deadDay: String?,
         /** 最終接続日時 */
         val lastAccessDatetime: LocalDateTime,
+        /** 最終接続日時（表示用） */
+        val lastAccess: String,
         /** 簡易メモ */
         val memo: String?
     ) {
@@ -41,7 +45,18 @@ data class VillageMemberContent(
             charaName = participant.name(),
             deadDay = if (participant.dead.isDead) "${participant.dead.deadDay}d" else null,
             lastAccessDatetime = participant.lastAccessDatetime,
+            lastAccess = mappingToLastAccess(participant.lastAccessDatetime),
             memo = participant.memo
         )
+
+        companion object {
+            private fun mappingToLastAccess(lastAccessDatetime: LocalDateTime): String {
+                val now = LocalDateTime.now()
+                val formatDateTime = lastAccessDatetime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"))
+                val hour = ChronoUnit.HOURS.between(lastAccessDatetime, now)
+                return if (hour < 1) "${formatDateTime}（1時間以内）"
+                else "${formatDateTime}（${hour}時間前）"
+            }
+        }
     }
 }
