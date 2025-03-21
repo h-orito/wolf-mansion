7745476cb166887a0fa0390ec2f73aa283fd3a8a
h-orito <16453967+h-orito@users.noreply.github.com>
2025-03-21 23:01:11 +0900

    add initial library

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  basePath: '/wolf-mansion',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
