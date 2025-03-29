2ffb7613ecf78d9369bbf36cf256694d7a8af8b0
h-orito <16453967+h-orito@users.noreply.github.com>
2025-03-29 17:11:45 +0900

    add myself api

--- backend/src/main/kotlin/com/ort/app/api/view/player/PlayerView.kt
+++ backend/src/main/kotlin/com/ort/app/api/view/player/PlayerView.kt
@@ -5,6 +5,11 @@ import com.ort.app.domain.model.player.Player
 data class PlayerView(
     val id: Int,
     val name: String,
+    val canCreateVillage: Boolean = false,
 ) {
-    constructor(org: Player) : this(id = org.id, name = org.name)
+    constructor(org: Player) : this(
+        id = org.id,
+        name = org.name,
+        canCreateVillage = org.isAvailableCreateVillage()
+    )
 }
